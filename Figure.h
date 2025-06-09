e66d2a1079d1c713648173fef4b233865321baa2
SoliBoy2006 <154845742+SoliBoy2006@users.noreply.github.com>
2025-06-09 14:09:56 +0300

    Update Figure.h

--- Figure.h
+++ Figure.h
@@ -1,4 +1,3 @@
-// Figure.h
 #pragma once
 
 #include <glm/glm.hpp>
@@ -7,13 +6,10 @@ class Figure {
 public:
     virtual ~Figure() = default;
 
-    // Проверка: находится ли точка внутри фигуры
     virtual bool isInside(const glm::vec2& pos) const = 0;
 
-    // Отразить скорость частицы при столкновении со стенкой
     virtual void reflect(glm::vec2& pos, glm::vec2& vel, float radius) const = 0;
 
-    // Рендер фигуры (опционально)
     virtual void render() const = 0;
 };
 
